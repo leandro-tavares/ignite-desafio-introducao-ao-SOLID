0ce345b98bca16a7961b0dafe41a34b3b4d682d5
Leandro Tavares <leandro@Mac-mini-de-Leandro.local>
2021-03-31 17:59:00 -0300

    Swagger implemented

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
 import express from "express";
+import morgan from "morgan";
+import swaggerUI from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerConfig from "./swagger.json";
+
+const swaggerOptions = {
+  customCssUrl: "/assets/swagger/custom.css",
+  customSiteTitle: "API User Documentation",
+  customfavIcon: "/assets/swagger/favicon.ico",
+};
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/assets", express.static("./src/assets"));
+
+app.use(
+  "/api/docs",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerConfig, swaggerOptions)
+);
 
 app.use("/users", usersRoutes);
 
